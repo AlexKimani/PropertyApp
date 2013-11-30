e91f6dd4d924bda8bf93970d3a47dcffa0d68b2f
dubdabasoduba <dubdabasoduba@gmail.com>
2013-11-30 15:56:35 +0300

    working on the registration file

--- src/com/propertyapp/Register.java
+++ src/com/propertyapp/Register.java
@@ -5,13 +5,50 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class Register extends Activity implements OnClickListener {
-	
+	 /**
+     *  JSON Response node names.
+     **/
+ 
+    private static String KEY_SUCCESS = "success";
+    private static String KEY_UID = "uid";
+    private static String KEY_FIRSTNAME = "user_fname";
+    private static String KEY_SECONDNAME = "user_sname";
+    private static String KEY_IDNO = "user_idno";
+    private static String KEY_PASSWORD = "user_password";
+    private static String KEY_OCCUPATION= "user_occupation";
+    private static String KEY_GENDER = "user_gender";
+    private static String KEY_ADDRESS = "user_address";
+    private static String KEY_EMAIL = "user_email";
+    private static String KEY_PHONENUMBER = "user_phonenumber";
+    private static String KEY_CREATED_AT = "created_at";
+    private static String KEY_ERROR = "error";
+    
+    EditText firstname, secondname,idnumber,password,occupation,address,email,phonenumber;
+    Spinner gender;
+	Button send;
+    
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.registration);
+		
+		//connect the variables to the fields
+		send=(Button)findViewById(R.id.submit);
+		firstname = (EditText)findViewById(R.id.fname);
+		secondname = (EditText)findViewById(R.id.sname);
+		idnumber = (EditText)findViewById(R.id.idno);
+		password = (EditText)findViewById(R.id.password);
+		occupation = (EditText)findViewById(R.id.occupation);
+		address = (EditText)findViewById(R.id.address);
+		email = (EditText)findViewById(R.id.email);
+		phonenumber = (EditText)findViewById(R.id.telephone);
+		gender =(Spinner)findViewById(R.id.spinner1);
+		
 	}
 	
 	@Override
