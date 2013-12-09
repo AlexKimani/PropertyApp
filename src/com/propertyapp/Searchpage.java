9cbf6fdba65424cf166faba27edbb08df9585121
dubdabasoduba <dubdabasoduba@gmail.com>
2013-12-09 14:30:07 +0300

    json files

--- src/com/propertyapp/Searchpage.java
+++ src/com/propertyapp/Searchpage.java
@@ -1,24 +1,19 @@
 package com.propertyapp;
 
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.List;
 
-import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-
+import org.json.JSONException;
+import org.json.JSONObject;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -32,7 +27,31 @@ import android.widget.Toast;
 public class Searchpage extends Activity implements OnClickListener{
     EditText idno,pword;
 	TextView changepass,register;
-	Button login;
+	Button login;;
+	private ProgressDialog pDialog;
+
+	// JSON parser class
+	JSONParser jsonParser = new JSONParser();
+
+	// php login script location:
+
+	// localhost :
+	// testing on your device
+	// put your local ip instead, on windows, run CMD > ipconfig
+	// or in mac's terminal type ifconfig and look for the ip under en0 or en1
+	// private static final String LOGIN_URL =
+	// "http://xxx.xxx.x.x:1234/webservice/login.php";
+
+	// testing on Emulator:
+	private static final String LOGIN_URL = "http://10.0.2.2/webservice/loginpge.php";
+
+	// testing from a real server:
+	// private static final String LOGIN_URL =
+	// "http://www.mybringback.com/webservice/login.php";
+
+	// JSON element ids from repsonse of php script:
+	private static final String TAG_SUCCESS = "success";
+	private static final String TAG_MESSAGE = "message";
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -53,45 +72,95 @@ public class Searchpage extends Activity implements OnClickListener{
 		register= (TextView)findViewById(R.id.register);
 		register.setOnClickListener(this);
 		}
-
-	public void  onClick(View v){
-
-		switch(v.getId()){
+	
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
 		case R.id.sub:
-			Intent l = new Intent(getApplicationContext(),Dashboard.class);
-			startActivity(l);
-			 /* if (  ( !idno.getText().toString().equals("")) && ( !pword.getText().toString().equals("")) )
-              {
-				 new login().execute();
-			          		         
-              }
-              else if ( ( !idno.getText().toString().equals("")) )
-              {
-                  Toast.makeText(getApplicationContext(),
-                          "Please fill in the Password field", Toast.LENGTH_SHORT).show();
-              }
-              else if ( ( !pword.getText().toString().equals("")) )
-              {
-                  Toast.makeText(getApplicationContext(),
-                          "Please fill in the ID Number field", Toast.LENGTH_SHORT).show();
-              }
-              else
-              {
-                  Toast.makeText(getApplicationContext(),
-                          "Please fill in the ID Number and Password fields", Toast.LENGTH_SHORT).show();
-              }	*/
-			  break;
-		case R.id.forgotpass:
-			Intent i = new Intent(getApplicationContext(), Updatepass.class);
-			startActivity(i);
+			new AttemptLogin().execute();
 			break;
 		case R.id.register:
-			Intent r = new Intent(getApplicationContext(), Register.class);
-			startActivity(r);
+			Intent i = new Intent(this, Register.class);
+			startActivity(i);
+			break;
+
+		default:
 			break;
 		}
-		
 	}
+
+	
+	class AttemptLogin extends AsyncTask<String, String, String> {
+
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			pDialog = new ProgressDialog(Searchpage.this);
+			pDialog.setMessage("Attempting login...");
+			pDialog.setIndeterminate(false);
+			pDialog.setCancelable(true);
+			pDialog.show();
+		}
+
+		@Override
+		protected String doInBackground(String... args) {
+			// TODO Auto-generated method stub
+			// Check for success tag
+			int success;
+			String user = idno.getText().toString();
+			String pass = pword.getText().toString();
+			try {
+				// Building Parameters
+				List<NameValuePair> params = new ArrayList<NameValuePair>(2);
+				params.add(new BasicNameValuePair("username", user));
+				params.add(new BasicNameValuePair("password", pass));
+
+				Log.d("request!", "starting");
+				// getting product details by making HTTP request
+				JSONObject json = jsonParser.makeHttpRequest(LOGIN_URL, "POST",	params);
+
+				// check your log for json response
+				Log.d("Login attempt", json.toString());
+
+				// json success tag
+				success = json.getInt(TAG_SUCCESS);
+				if (success == 1) {
+					Log.d("Login Successful!", json.toString());
+/*					// save user data
+					SharedPreferences sp = PreferenceManager
+							.getDefaultSharedPreferences(Searchpage.this);
+					Editor edit = sp.edit();
+					edit.putString("username", username);
+					edit.commit();
+					*/
+					Intent i = new Intent(Searchpage.this, Dashboard.class);
+					finish();
+					startActivity(i);
+					return json.getString(TAG_MESSAGE);
+				} else {
+					Log.d("Login Failure!", json.getString(TAG_MESSAGE));
+					return json.getString(TAG_MESSAGE);
+				}
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+
+			return null;
+
+		}
+
+		protected void onPostExecute(String file_url) {
+			// dismiss the dialog once product deleted
+			pDialog.dismiss();
+			if (file_url != null) {
+				Toast.makeText(Searchpage.this, file_url, Toast.LENGTH_LONG).show();
+			}
+
+		}
+
+	}
+	
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
@@ -100,78 +169,6 @@ public class Searchpage extends Activity implements OnClickListener{
 		return true;
 	}
 
-	
-	private class login extends AsyncTask<Object, Object, Object>{
-		String id = idno.getText().toString();
-		String pass = pword.getText().toString();
-		 private ProgressDialog pDialog;
-		 @Override
-         protected void onPreExecute() {
-             super.onPreExecute();
-             pDialog = new ProgressDialog(Searchpage.this);
-             pDialog.setTitle("Contacting Servers");
-             pDialog.setMessage("Logining in...");
-             pDialog.setIndeterminate(false);
-             pDialog.setCancelable(true);
-             pDialog.show();
-         }
-		@Override
-		protected Object doInBackground(Object... params) {
-//			  What the application should do after the checking					  
-			  try {
-		    		
-		        	//create a http default client - initialize the HTTp client
-		          DefaultHttpClient httpclient = new DefaultHttpClient();				            
-		         HttpPost httppost = new HttpPost("http://10.0.2.2/Propertyapp/login.php");
-		         ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(2); 
-		           nameValuePairs.add(new BasicNameValuePair("use", id));
-		           nameValuePairs.add(new BasicNameValuePair("pas", pass));	       
-		           httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-		            // Execute HTTP Post Request
-		           HttpResponse response = httpclient.execute(httppost);				            
-		            InputStream inputStream = response.getEntity().getContent();		            
-		            BufferedReader rd = new BufferedReader(new InputStreamReader(inputStream), 4096);
-		            String line;
-		            StringBuilder sb =  new StringBuilder();		            
-		            while ((line = rd.readLine()) != null) {
-		            		sb.append(line);
-		            }
-		            rd.close();
-		            String result = sb.toString();				            
-		            inputStream.close();
-		         if(result.matches("2")){
-		        	 pDialog.dismiss();
-		        		        	 
-		         }
-		         else {
-		           // String  password = result;
-		            if (result.matches(pass)){					            
-			            Intent intent=new Intent(Searchpage.this,Dashboard.class);
-						Bundle bundle = new Bundle();
-						bundle.putString("landlordid", id);	//keyword- selected clinic	
-						intent.putExtras(bundle);
-					    startActivity(intent);
-					    pDialog.dismiss();
-		            }
-		           else if(result!= pass){
-		        	   pDialog.dismiss();
-		        	  /*Toast.makeText(Searchpage.this, "Incorrect password please check password", Toast.LENGTH_LONG).show();*/
-		            }
-		           
-		         }
-		         }           
-		            catch (Exception e)
-		            {
-		            	pDialog.dismiss();
-		                Toast.makeText(Searchpage.this, "Error inside set:"+e.toString(), Toast.LENGTH_LONG).show();
-		            }
-			return null;
-			  
-//			  End of the the work of the code
-			  
-		}
-		
-	}
 	 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
@@ -191,7 +188,7 @@ public class Searchpage extends Activity implements OnClickListener{
 	            return super.onOptionsItemSelected(item);
 	    }
 	}
-	
+
 	}
 	 
 
