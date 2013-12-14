1f76ecf1a3a103e005804c441f1bae36cba7dd50
dubdabasoduba <dubdabasoduba@gmail.com>
2013-12-14 12:11:27 +0300

    working list fetch

---
+++ src/com/propertyapp/ApplicationAdapter.java
@@ -0,0 +1,57 @@
+package com.propertyapp;
+
+import java.util.List;
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+public class ApplicationAdapter extends ArrayAdapter<Application> {
+
+	 private List<Application> items;
+	    
+	    public ApplicationAdapter(Context context, List<Application> items) {
+	        super(context, R.layout.single_post, items);
+	        this.items = items;
+	    }
+	    
+	    @Override
+	    public int getCount() {
+	        return items.size();
+	    }
+	    
+	    @Override
+	    public View getView(int position, View convertView, ViewGroup parent) {
+	        View v = convertView;
+	        
+	        if(v == null) {
+	            LayoutInflater li = LayoutInflater.from(getContext());
+	            v = li.inflate(R.layout.single_post, null);            
+	        }
+	        
+	        Application app = items.get(position);
+	        
+	        if(app != null) {
+	            TextView pname = (TextView)v.findViewById(R.id.propertyname);
+	            TextView psize = (TextView)v.findViewById(R.id.propertysize);
+	            TextView pprice = (TextView)v.findViewById(R.id.propertyprice);
+	            TextView plocation = (TextView)v.findViewById(R.id.location);
+	            TextView uname = (TextView)v.findViewById(R.id.username);
+	            TextView utel = (TextView)v.findViewById(R.id.usertelephone);
+	            TextView uemail = (TextView)v.findViewById(R.id.useremail);
+	           
+	            
+	            if(pname != null) pname.setText("Property Name:  "+app.getProperty_name());
+	            if(psize != null) psize.setText("Property Size:  "+app.getProperty_size());
+	            if(pprice != null) pprice.setText("Property Price:  "+app.getProperty_price());
+	            if(plocation != null) plocation.setText("Property Location:  "+app.getProperty_location());
+	            if(uname != null) uname.setText("Owner Username:  "+app.getUser_username());
+	            if(utel != null) utel.setText("Owner Phonumber:  "+app.getUser_phonenumber());
+	            if(uemail != null)uemail.setText("Owner Email:  "+app.getUser_email());
+	            }
+	                
+	        return v;
+	    }
+	
+}
--- src/com/propertyapp/Dashboard.java
+++ src/com/propertyapp/Dashboard.java
@@ -1,40 +1,26 @@
 package com.propertyapp;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import org.apache.http.NameValuePair;
-import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import android.app.Activity;
-import android.app.ProgressDialog;
 import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
-import android.widget.AdapterView.OnItemSelectedListener;
 
 public class Dashboard extends Activity implements OnClickListener{
 	Button send;
 	EditText value,place;
 	Spinner type;
 	
-	 // Progress Dialog
-    private ProgressDialog pDialog;
- 
-    // JSON parser class
+	 // JSON parser class
     JSONParser jsonParser = new JSONParser();
     
     //php register script
@@ -45,19 +31,7 @@ public class Dashboard extends Activity implements OnClickListener{
     //or in mac's terminal type ifconfig and look for the ip under en0 or en1
    // private static final String REGISTER_URL = "http://xxx.xxx.x.x:1234/webservice/register.php";
     
-    //testing on Emulator:
-    private static final String LAND_URL = "http://10.0.2.2/Propertyapp/comments.php";
-    private static final String RESHOUSE_URL = "http://10.0.2.2/Propertyapp/reshouse.php";
-    private static final String COMMHOUSE_URL = "http://10.0.2.2/Propertyapp/commhouse.php";
-  //testing from a real server:
-    //private static final String REGISTER_URL = "http://www.mybringback.com/webservice/register.php";
-    
-    //ids
-    private static final String TAG_SUCCESS = "success";
-    private static final String TAG_MESSAGE = "message";
-	
-	
-	@Override
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.dashboard);
@@ -77,7 +51,7 @@ public class Dashboard extends Activity implements OnClickListener{
 
 				@Override
 				public void onItemSelected(AdapterView<?> parent, View arg1,int arg2, long arg3) {
-					String item = (String) parent.getItemAtPosition(arg2);	
+					parent.getItemAtPosition(arg2);	
 					return;
 				}
 
@@ -87,227 +61,32 @@ public class Dashboard extends Activity implements OnClickListener{
 					
 				}
 				
-			});     	
+			});
 		}
 
 
 	public void onClick(View v) {
 		if(type.getSelectedItem().equals("Land")){
+		String	val= value.getText().toString();
+		String  plc= place.getText().toString();
 			Intent i = new Intent(Dashboard.this, Results.class);
-			/*finish();*/
+			Bundle bundle = new Bundle();
+			bundle.putString("msg1",val);
+			bundle.putString("msg2", plc);
+			i.putExtras(bundle);
 			startActivity(i);
-			/*new findland().execute();*/
 		}
 		else if(type.getSelectedItem().equals("Commercial House")){
-			new findcommhouse().execute();
+			
 		}
 		else if(type.getSelectedItem().equals("Residential House")){
-			new findreshouse().execute();
+			
 		}
 		else
 		{
 			Toast.makeText(Dashboard.this, "Please select property type", Toast.LENGTH_LONG).show();
 		}
 	}
-	
-class findland extends AsyncTask<String, String, String> {
-
-		
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            pDialog = new ProgressDialog(Dashboard.this);
-            pDialog.setTitle("Contacting Servers");
-            pDialog.setMessage("Please wait...");
-            pDialog.setIndeterminate(false);
-            pDialog.setCancelable(true);
-            pDialog.show();
-        }
-		
-		@Override
-		protected String doInBackground(String... args) {
-			// TODO Auto-generated method stub
-			 // Check for success tag
-            int success;
-            String loc = place.getText().toString();
-            String val = value.getText().toString();
-           
-            try {
-                // Building Parameters
-                List<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("price", val));
-                params.add(new BasicNameValuePair("location",loc));  
- 
-                Log.d("request!", "starting");
-                
-                //Posting user data to script 
-                JSONObject json = jsonParser.makeHttpRequest( LAND_URL, "POST", params);
- 
-                // full json response
-                Log.d("Registering attempt", json.toString());
- 
-                // json success element
-                success = json.getInt(TAG_SUCCESS);
-                if (success == 1) {
-					Intent i = new Intent(Dashboard.this, Results.class);
-					finish();
-					startActivity(i);
-                }else{
-                	Log.d("Searching Failure!", json.getString(TAG_MESSAGE));
-                	return json.getString(TAG_MESSAGE);
-                	
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
- 
-            return null;
-			
-		}
-		
-        protected void onPostExecute(String file_url) {
-            // dismiss the dialog once product deleted
-            pDialog.dismiss();
-            if (file_url != null){
-            	Toast.makeText(Dashboard.this, file_url, Toast.LENGTH_LONG).show();
-            }
- 
-        }
-		
-	}
-
-class findreshouse extends AsyncTask<String, String, String> {
-
-	
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        pDialog = new ProgressDialog(Dashboard.this);
-        pDialog.setTitle("Contacting Servers");
-        pDialog.setMessage("Please wait...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(true);
-        pDialog.show();
-    }
-	
-	@Override
-	protected String doInBackground(String... args) {
-		// TODO Auto-generated method stub
-		 // Check for success tag
-        int success;
-        String loc = place.getText().toString();
-        String val = value.getText().toString();
-       
-        try {
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("price", val));
-            params.add(new BasicNameValuePair("location",loc));  
-
-            Log.d("request!", "starting");
-            
-            //Posting user data to script 
-            JSONObject json = jsonParser.makeHttpRequest( RESHOUSE_URL, "POST", params);
-
-            // full json response
-            Log.d("Registering attempt", json.toString());
-
-            // json success element
-            success = json.getInt(TAG_SUCCESS);
-            if (success == 1) {              	
-				Intent i = new Intent(Dashboard.this, Results.class);
-				finish();
-				startActivity(i);
-            }else{
-            	Log.d("Searching Failure!", json.getString(TAG_MESSAGE));
-            	return json.getString(TAG_MESSAGE);
-            	
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-		
-	}
-	
-    protected void onPostExecute(String file_url) {
-        // dismiss the dialog once product deleted
-        pDialog.dismiss();
-        if (file_url != null){
-        	Toast.makeText(Dashboard.this, file_url, Toast.LENGTH_LONG).show();
-        }
-
-    }
-	
-}
-
-
-class findcommhouse extends AsyncTask<String, String, String> {
-
-	
-    @Override
-    protected void onPreExecute() {
-        super.onPreExecute();
-        pDialog = new ProgressDialog(Dashboard.this);
-        pDialog.setTitle("Contacting Servers");
-        pDialog.setMessage("Please wait...");
-        pDialog.setIndeterminate(false);
-        pDialog.setCancelable(true);
-        pDialog.show();
-    }
-	
-	@Override
-	protected String doInBackground(String... args) {
-		// TODO Auto-generated method stub
-		 // Check for success tag
-        int success;
-        String loc = place.getText().toString();
-        String val = value.getText().toString();
-       
-        try {
-            // Building Parameters
-            List<NameValuePair> params = new ArrayList<NameValuePair>();
-            params.add(new BasicNameValuePair("price", val));
-            params.add(new BasicNameValuePair("location",loc));  
-
-            Log.d("request!", "starting");
-            
-            //Posting user data to script 
-            JSONObject json = jsonParser.makeHttpRequest( COMMHOUSE_URL, "POST", params);
-
-            // full json response
-            Log.d("Registering attempt", json.toString());
-
-            // json success element
-            success = json.getInt(TAG_SUCCESS);
-            if (success == 1) {              	
-				Intent i = new Intent(Dashboard.this, Results.class);
-				finish();
-				startActivity(i);
-            }else{
-            	Log.d("Searching Failure!", json.getString(TAG_MESSAGE));
-            	return json.getString(TAG_MESSAGE);
-            	
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return null;
-		
-	}
-	
-    protected void onPostExecute(String file_url) {
-        // dismiss the dialog once product deleted
-        pDialog.dismiss();
-        if (file_url != null){
-        	Toast.makeText(Dashboard.this, file_url, Toast.LENGTH_LONG).show();
-        }
-
-    }
-	
-}
 	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
