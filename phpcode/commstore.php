01551a136559291934c3c58c0725647234a5b18e
dubdabasoduba <dubdabasoduba@gmail.com>
2013-12-15 13:03:27 +0300

    commercial house php scrit edited

--- phpcode/commstore.php
+++ phpcode/commstore.php
@@ -158,6 +158,28 @@ if (!empty($_POST)) {
         ':nom' => $_POST['Bnumber'],
         ':purp' => $_POST['purpose']
     );
+       
+    //time to run our query, and create the user
+    try {
+        $stmt   = $db->prepare($query2);
+        $result = $stmt->execute($query_params);
+    }
+    catch (PDOException $ex) {
+        // For testing, you could use a die and message. 
+        //die("Failed to run query: " . $ex->getMessage());
+        
+        //or just use this use this one:
+        $response["success"] = 0;
+        $response["message"] = "Database Error5. Please Try Again!";
+        die(json_encode($response));
+    }
+    
+    //If we have made it this far without dying, we have successfully added
+    //a new user to our database.  We could do a few things here, such as 
+    //redirect to the login page.  Instead we are going to echo out some
+    //json data that will be read by the Android application, which will login
+    //the user (or redirect to a different activity, I'm not sure yet..)
+    $response["success"] = 1;
     $response["message"] = "commercial house Registered";
     echo json_encode($response);
     
